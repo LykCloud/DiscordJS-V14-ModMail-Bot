9f1469977afa13440346d4145cd5fb80bfb54898
LykCloud <145738195+LykCloud@users.noreply.github.com>
2023-09-22 21:08:35 +0530

    Update and rename example.config.js to config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    client: {
+        token: '', // ← Your bot token (DO NOT USE IN REPLIT)
+        id: '1154803796636094547' // ← Your bot ID
+    },
+    modmail: {
+        guildId: '1143116469866090506', // ← Your server ID
+        categoryId: '1154803457878917150', // ← The modmail category ID
+        staffRoles: ['1143116470029656073'], // ← The modmail staff roles IDs
+        mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
+    }
+};
